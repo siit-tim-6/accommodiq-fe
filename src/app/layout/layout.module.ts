0c2a6c6c32fffa8667b72d7b997eabd1821651da
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-11-25 00:48:11 +0100

    feat: Initialize login component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, LoginComponent],
   imports: [CommonModule],
-  exports: [NavBarComponent],
+  exports: [NavBarComponent, LoginComponent],
 })
 export class LayoutModule {}
---
+++ src/app/layout/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  onSubmit() {
+    alert("Submit pressed!");
+  }
+}
