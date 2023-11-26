e9b6ae0188df0739d8a03d7f2425201953e5bbd3
Teodor Vidakovic <teodorv22@gmail.com>
2023-11-26 21:16:48 +0100

    feat: add routes for login and registration component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './layout/login/login.component';
+import { RegistrationComponent } from './layout/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
