365c10e05d8fffdbd19bfdbc6f43852f1dc3368d
Vladimir Popov <vladapopov02@gmail.com>
2023-11-27 17:42:38 +0100

    feat: add accommodation-search component

---
+++ src/app/accommodation/accommodation-search/accommodation-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accommodation-search',
+  templateUrl: './accommodation-search.component.html',
+  styleUrl: './accommodation-search.component.css'
+})
+export class AccommodationSearchComponent {
+
+}
