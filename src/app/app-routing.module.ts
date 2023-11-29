6a8dc657ff9a9759d9d1a31815b73c6ed30aaf0c
Teodor Vidakovic <teodorv22@gmail.com>
2023-11-30 00:55:41 +0100

    feat: apply PR suggestion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './layout/login/login.component';
 import { RegistrationComponent } from './layout/registration/registration.component';
+import {AccountInfoComponent} from "./layout/account-info/account-info.component";
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'registration', component: RegistrationComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'account-info', component: AccountInfoComponent },
 ];
 
 @NgModule({
