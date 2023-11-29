79dcb5e173100c0e63ef509d90eebe640ab89ab6
Vladimir Popov <vladapopov02@gmail.com>
2023-11-30 00:58:33 +0100

    feat: fix app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AccommodationModule } from './accommodation/accommodation.module';
-import { CommentCardComponent } from './comment/comment-card/comment-card.component';
-import {RatingModule} from "primeng/rating";
-import { CommentListComponent } from './comment/comment-list/comment-list.component';
-import { AddCommentFormComponent } from './comment/add-comment-form/add-comment-form.component';
-import {InputTextModule} from "primeng/inputtext";
-import {ButtonModule} from "primeng/button";
 
 @NgModule({
-  declarations: [AppComponent, CommentCardComponent, CommentListComponent, AddCommentFormComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -24,9 +18,6 @@ import {ButtonModule} from "primeng/button";
     AccommodationModule,
     FormsModule,
     ReactiveFormsModule,
-    RatingModule,
-    InputTextModule,
-    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
