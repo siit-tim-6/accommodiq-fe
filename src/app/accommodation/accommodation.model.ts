ac90b461a6921a412da13665649cb562ceef1d00
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-12-04 01:37:47 +0100

    feat: Create Accommodation interface

---
+++ src/app/accommodation/accommodation.model.ts
@@ -0,0 +1,10 @@
+export interface Accommodation {
+  id: number;
+  title: string;
+  rating: number;
+  reviewCount: number;
+  location: string;
+  minPrice: number;
+  minGuests: number;
+  maxGuests: number;
+}
