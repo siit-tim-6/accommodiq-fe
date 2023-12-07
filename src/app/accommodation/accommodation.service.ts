968dd622404d5437215a2726927e8152ec6f4abf
Vladimir Popov <vladapopov02@gmail.com>
2023-12-07 01:17:48 +0100

    feat: add service method for filtering and searching

--- src/app/accommodation/accommodation.service.ts
+++ src/app/accommodation/accommodation.service.ts
@@ -1,27 +1,43 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {environment} from "../../env/env";
-import {Accommodation} from "./accommodation.model";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../env/env';
+import { Accommodation } from './accommodation.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccommodationService {
   private accommodationList: Accommodation[] = [];
 
-  constructor(private httpClient: HttpClient) {
-  }
+  constructor(private httpClient: HttpClient) {}
 
   getAll(): Observable<Accommodation[]> {
-    return this.httpClient.get<Accommodation[]>(environment.apiHost + 'accommodations')
+    return this.httpClient.get<Accommodation[]>(
+      environment.apiHost + 'accommodations'
+    );
   }
 
   getAccommodation(id: number): Observable<Accommodation> {
-    return this.httpClient.get<Accommodation>(environment.apiHost + 'accommodations/' + id)
+    return this.httpClient.get<Accommodation>(
+      environment.apiHost + 'accommodations/' + id
+    );
   }
 
   getHostsAccommodations(): Observable<Accommodation[]> {
-    return this.httpClient.get<Accommodation[]>(environment.apiHost + 'hosts/' + 1 + "/accommodations") // change later with JWT
+    return this.httpClient.get<Accommodation[]>(
+      environment.apiHost + 'hosts/' + 1 + '/accommodations'
+    ); // change later with JWT
+  }
+
+  findByFilter(
+    location: string,
+    fromDate: number,
+    toDate: number,
+    title: string
+  ): Observable<Accommodation[]> {
+    return this.httpClient.get<Accommodation[]>(
+      `${environment.apiHost}accommodations?location=${location}&availableFrom=${fromDate}&availableTo=${toDate}&title=${title}`
+    );
   }
 }
