65d49c887a8d812a05ff6b42f3ec066aecaddf39
Vladimir Popov <vladapopov02@gmail.com>
2023-12-07 02:04:15 +0100

    feat: add event, models and outputs to accommodation-search component

--- src/app/accommodation/accommodation-search/accommodation-search.component.ts
+++ src/app/accommodation/accommodation-search/accommodation-search.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { SearchParams } from '../search-params.model';
 
 @Component({
   selector: 'app-accommodation-search',
   templateUrl: './accommodation-search.component.html',
-  styleUrl: './accommodation-search.component.css'
+  styleUrl: './accommodation-search.component.css',
 })
 export class AccommodationSearchComponent {
+  location: string = '';
+  title: string = '';
+  rangeDates: Date[] | undefined;
 
+  @Output()
+  onSearch = new EventEmitter<SearchParams>();
+
+  @Output()
+  onClear = new EventEmitter<never>();
+
+  search() {
+    this.onSearch.emit({
+      title: this.title,
+      location: this.location,
+      rangeDates: this.rangeDates,
+    });
+  }
+
+  clear() {
+    this.location = '';
+    this.title = '';
+    this.rangeDates = undefined;
+    this.onClear.emit();
+  }
 }
