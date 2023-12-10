a93100b2c9088f8aa663cac16fe26e90eb7ec2ce
Vladimir Popov <vladapopov02@gmail.com>
2023-12-10 02:02:54 +0100

    feat: add accommodation type dropdown

--- src/app/accommodation/accommodation-search/accommodation-search.component.ts
+++ src/app/accommodation/accommodation-search/accommodation-search.component.ts
@@ -7,6 +7,18 @@ import { SearchParams } from '../search-params.model';
   styleUrl: './accommodation-search.component.css',
 })
 export class AccommodationSearchComponent {
+  accommodationTypes: String[] = [
+    'Luxurious Hotel Suite',
+    'Cozy Mountain Cabin',
+    'Beachfront Villa',
+    'Trendy Urban Loft',
+    'Quaint Bed and Breakfast',
+    'Modern City Apartment',
+    'Rustic Country Cottage',
+    'Boutique Guesthouse',
+    'Treehouse Retreat',
+    'Floating Houseboat',
+  ];
   location: string = '';
   title: string = '';
   rangeDates: Date[] | undefined;
