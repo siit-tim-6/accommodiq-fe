15dc2808916dac0875dcbc4398458ca8b11963c0
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-12-17 01:19:38 +0100

    feat: Apply suggestions

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -6,18 +6,18 @@ import {Injectable} from "@angular/core";
 @Injectable({
   providedIn: 'root',
 })
-export class TemplateService {
+export class RestService {
   constructor(private httpClient: HttpClient) {}
 
   getApiUrl(endpoint: string): string {
     return environment.apiHost + endpoint;
   }
 
-  getObservable<T>(endpoint: string): Observable<T> {
+  get<T>(endpoint: string): Observable<T> {
     return this.httpClient.get<T>(this.getApiUrl(endpoint));
   }
 
-  putObservable<T>(endpoint: string, body: any): Observable<HttpResponse<T>> {
+  put<T>(endpoint: string, body: any): Observable<HttpResponse<T>> {
     return this.httpClient.put<HttpResponse<T>>(this.getApiUrl(endpoint), body);
   }
 }
