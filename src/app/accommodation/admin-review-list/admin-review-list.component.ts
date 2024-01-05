88ac39fc8c6a2082e83654c7eaade343e2660d8e
Vladimir Popov <vladapopov02@gmail.com>
2024-01-05 12:23:13 +0100

    feat: add map to admin-review-list

--- src/app/accommodation/admin-review-list/admin-review-list.component.ts
+++ src/app/accommodation/admin-review-list/admin-review-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Accommodation } from '../accommodation.model';
 import { AccommodationService } from '../accommodation.service';
+import { HttpClient, JsonpClientBackend } from '@angular/common/http';
+import { Observable, catchError, map, of } from 'rxjs';
+import { keys } from '../../../env/keys';
 
 @Component({
   selector: 'app-admin-review-list',
@@ -9,9 +12,28 @@ import { AccommodationService } from '../accommodation.service';
     '../hosts-accommodation-list/hosts-accommodation-list.component.css',
 })
 export class AdminReviewListComponent implements OnInit {
+  private httpClient: HttpClient;
   accommodations: Accommodation[] = [];
+  apiLoaded: Observable<boolean>;
 
-  constructor(private service: AccommodationService) {}
+  constructor(
+    private service: AccommodationService,
+    private httpBackend: JsonpClientBackend,
+  ) {
+    this.httpClient = new HttpClient(httpBackend);
+    this.apiLoaded = this.httpClient
+      .jsonp(
+        `https://maps.googleapis.com/maps/api/js?key=${keys.googleMaps}`,
+        'callback',
+      )
+      .pipe(
+        map(() => true),
+        catchError((error) => {
+          console.log(error);
+          return of(false);
+        }),
+      );
+  }
 
   ngOnInit(): void {
     this.service
