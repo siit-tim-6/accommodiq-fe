8719a8c6d75e56fbd3387343812cc553daa8497c
Vladimir Popov <vladapopov02@gmail.com>
2024-01-06 22:51:10 +0100

    feat: implement reservation service method for getting all accommodations

--- src/app/reservation/reservation.model.ts
+++ src/app/reservation/reservation.model.ts
@@ -1,6 +1,21 @@
+import { AccommodationLocation } from '../accommodation/accommodation.model';
+
 export enum ReservationStatus {
   ACCEPTED = 'ACCEPTED',
   PENDING = 'PENDING',
   CANCELLED = 'CANCELLED',
   DECLINED = 'DECLINED',
 }
+
+export type Reservation = {
+  accommodationImage: string;
+  accommodationTitle: string;
+  accommodationRating: number;
+  accommodationReviewCount: number;
+  accommodationLocation: AccommodationLocation;
+  guests: number;
+  startDate: number;
+  endDate: number;
+  status: ReservationStatus;
+  totalPrice: number;
+};
--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,8 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Reservation } from './reservation.model';
+import { environment } from '../../env/env';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ReservationService {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
+
+  findByFilter(): Observable<Reservation[]> {
+    return this.httpClient.get<Reservation[]>(
+      `${environment.apiHost}guests/reservations`,
+    );
+  }
 }
