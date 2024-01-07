3fdf5307f3e42af46bd3319f7f76d49884c16386
Vladimir Popov <vladapopov02@gmail.com>
2024-01-08 00:40:33 +0100

    feat: add route guard for guest reservation-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -48,6 +48,7 @@ const routes: Routes = [
   {
     path: 'my-reservations',
     component: ReservationListComponent,
+    canActivate: [() => CanActivateRole(AccountRole.GUEST)],
   },
   {
     path: 'profile-account/:accountId',
