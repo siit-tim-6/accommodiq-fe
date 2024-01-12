931e658e594d8386d7667fc153f21f566a732c65
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-01-12 03:14:15 +0100

    refactor: Remove unused method

--- src/app/infrastructure/auth/guards/role.guard.ts
+++ src/app/infrastructure/auth/guards/role.guard.ts
@@ -2,26 +2,13 @@ import { Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AccountRole } from '../../../account/account-info/account.model';
 import { JwtService } from '../jwt.service';
-
-export const CanActivateRole = (role: AccountRole[]) => {
-  const jwtService = inject(JwtService);
-  const router = inject(Router);
-
-  let token = localStorage.getItem('user');
-  if (token === null || !role.includes(jwtService.getRole()!)) {
-    router.navigate(['/search']); //unauthorized
-    return false;
-  }
-  return true;
-};
-
 export const CanActivateRoles = (roles: AccountRole[]) => {
   const jwtService = inject(JwtService);
   const router = inject(Router);
 
   let role = jwtService.getRole();
   if (role === null || !roles.includes(role)) {
-    router.navigate(['/search']);
+    router.navigate(['/search']).then((_) => {});
     return false;
   }
   return true;
