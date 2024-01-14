0628b0e1af2b723254ca6b724b498c5ff0fc973d
Vladimir Popov <vladapopov02@gmail.com>
2024-01-14 19:13:29 +0100

    feat: implement card component

--- src/app/financial-report/financial-report-card/financial-report-card.component.ts
+++ src/app/financial-report/financial-report-card/financial-report-card.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FinancialReportEntry } from '../financial-report.model';
+import { environment } from '../../../env/env';
 
 @Component({
   selector: 'app-financial-report-card',
   templateUrl: './financial-report-card.component.html',
   styleUrl: './financial-report-card.component.css',
 })
-export class FinancialReportCardComponent {}
+export class FinancialReportCardComponent {
+  @Input() entry!: FinancialReportEntry;
+  protected imageBase = environment.imageBase;
+}
