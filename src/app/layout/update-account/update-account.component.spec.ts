82a6620f963c2100fd32f6aec5420d48ebe3f6b8
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-01-23 09:34:08 +0100

    feat: revert more tests

--- src/app/layout/update-account/update-account.component.spec.ts
+++ src/app/layout/update-account/update-account.component.spec.ts
@@ -29,22 +29,11 @@ describe('UpdateAccountComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should disable the button when the form is invalid', () => {
-    component.accountDetails = accountDetails;
-    component.newPassword = '123456';
-    component.accountDetails.firstName = '';
-
-    fixture.detectChanges();
-
-    const button = fixture.debugElement.query(By.css('#personal-data-button'));
-    expect(button).toBeTruthy(); // Check if the button element is found
-    expect(button.nativeElement.disabled).toBeTruthy();
-  });
-
   it('should disable the button when the passwords are empty', () => {
     component.accountDetails = accountDetails;
     component.newPassword = '';
     component.repeatNewPassword = '';
+    component.oldPassword = '12345';
 
     fixture.detectChanges();
 
@@ -89,5 +78,39 @@ describe('UpdateAccountComponent', () => {
     const button = fixture.debugElement.query(By.css('#password-button'));
     expect(button).toBeTruthy(); // Check if the button element is found
     expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+  it('should disable the button when the old password is empty', () => {
+    component.accountDetails = accountDetails;
+    component.newPassword = '123456';
+    component.repeatNewPassword = '123456';
+    component.oldPassword = '';
+
+    fixture.detectChanges();
+
+    const button = fixture.debugElement.query(By.css('#password-button'));
+    expect(button).toBeTruthy(); // Check if the button element is found
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+  it('should disable the button when the form is invalid', () => {
+    component.accountDetails = accountDetails;
+    component.accountDetails.firstName = '';
+
+    fixture.detectChanges();
+
+    const button = fixture.debugElement.query(By.css('#personal-data-button'));
+    expect(button).toBeTruthy(); // Check if the button element is found
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+  it('should enable the button when the form is valid', () => {
+    component.accountDetails = accountDetails;
+
+    fixture.detectChanges();
+
+    const button = fixture.debugElement.query(By.css('#personal-data-button'));
+    expect(button).toBeTruthy(); // Check if the button element is found
+    expect(button.nativeElement.disabled).toBeFalsy();
   });
 });
